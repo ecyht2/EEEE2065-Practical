5876df35d2fb2262a9f4ad8b83cfcaba15098fd1
ecyht2 <ecyht2@nottingham.edu.my>
2022-10-11 17:14:08 +0800

    Fixed C2F and F2C formatting

--- Practical-2/testMath.cpp
+++ Practical-2/testMath.cpp
@@ -24,20 +24,20 @@ double convertC2F(double C) {
 
 /** Shows Temperature in Farenheit and Celcius from 0 F to 100 F */
 void F2C() {
-    cout << setw(5);
-    cout << "F" << setw(5) << "C" << endl;
+    cout << setw(3) << "F";
+    cout << setw(8) << "C" << endl;
     for (double F = 0.0; F <= 100.0; F+=20.00) {
-        cout << setprecision(4);
-        cout << setw(5) << F << setw(5) << convertF2C(F) << endl;
+        cout << setprecision(5);
+        cout << setw(3) << F << setw(8) << convertF2C(F) << endl;
     }
 }
 
 /** Shows Temperature in Celius and Farenheit from 0 C to 100 C */
 void C2F() {
-    cout << setw(5);
+    cout << setw(3);
     cout << "C" << setw(5) << "F" << endl;
     for (double C = 0.0; C <= 100.00; C+=20.0) {
-        cout << setprecision(4);
-        cout << setw(5) << C << setw(5) << convertC2F(C) << endl;
+        cout << setprecision(5);
+        cout << setw(3) << C << setw(5) << convertC2F(C) << endl;
     }
 }
